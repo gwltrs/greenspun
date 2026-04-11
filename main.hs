f0d3d6bf399ed6464dcd1eb6ba384d3211220716
Gregory Walters <gregorywalters@Gregorys-MacBook-Pro.local>
2026-04-11 12:28:57 -0400

    parsing vars

--- main.hs
+++ main.hs
@@ -1,20 +1,37 @@
+{-# LANGUAGE LambdaCase #-}
+
 import Control.Applicative
 import Parsers
-import Data.Set
-import Data.List 
+import Data.List
+import Data.Maybe (fromMaybe)
+import Data.Set (Set)
 import Utils
 
-data CompileError = SyntaxError
-data Var = Var { name :: String, type_ :: Sexp }
+data CompileError = SyntaxError | MismatchError
+data Var = Var { name :: String, type_ :: Sexp, body :: Sexp } deriving Show
 newtype Env = Env { variables :: Set Var }
 
-var :: Sexp -> Either (Maybe CompileError) Var
-var (Atom _) = Left Nothing
-var (List sexps) = 
-    if (sexps !? 0) /= (Justt $ Atom "var") then Left Nothing else
-    if (length sexps)
-    if (sexps !? 1) /= (Just $ Atom "var") then Nothing else
-    Nothing
+flatSymbols :: Sexp -> Maybe [String]
+flatSymbols (List l) = mapM (\case (List l') -> Nothing; (Atom s') -> Just s') l
+flatSymbols (Atom s) = Just [s]
+
+-- isList :: Sexp -> Bool
+-- isList (List _) = True
+-- isList _ = False
+
+var :: Sexp -> Maybe [Var]
+var (Atom _) = Just []
+var (List ((Atom "var") : sexps)) = 
+    let len = length sexps; names = fromMaybe [] $ flatSymbols (sexps !! 1) in
+    if len < 2 || len > 3 || null names then Nothing else
+    let body = fromMaybe (List [Atom "default"]) (sexps !? 2) in
+    Just ((\n -> Var { name = n, type_ = head sexps, body = body }) <$> names)
+var _ = Just []
+
+-- var (List [Atom "var", type_, Atom name]) = var $ List [Atom "var", type_, Atom name, List [Atom "default"]]
+
+-- var (List [Atom "var", type_, Atom name, body]) = 
+-- Nothing
 
 globalEnv :: [Sexp] -> Env
 globalEnv = undefined
