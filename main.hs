7556af7cc275efb4bd0a656e6bbf254b83ba29ca
Gregory Walters <gregorywalters@Gregorys-MacBook-Pro.local>
2026-05-12 07:59:35 -0400

    transpile.hs

--- main.hs
+++ main.hs
@@ -20,6 +20,7 @@ import Type.CompileResult
 import Distribution.Simple.Utils (safeLast, safeInit)
 import Data.Bifunctor (second)
 import Data.Char (ord)
+import Transpile
 
 filePathSexps :: FilePath -> IO (Maybe [Sexp])
 filePathSexps path = do
@@ -34,137 +35,6 @@ greenFilesSexps = do
     sexps <- traverse filePathSexps paths
     pure $ concat <$> sequence sexps
 
-transpileType :: Sexp -> String
-transpileType (Atom "Void") = "void"
-transpileType (Atom "Bool") = "bool"
-transpileType (Atom "Char") = "char"
-transpileType (Atom "I32") = "int"
-transpileType (Atom "F32") = "float"
-transpileType (Atom t) = t
-transpileType (List [Atom "*", inner]) = transpileType inner ++ "*"
--- transpileType (List [Atom "->", inner]) = 
-
-transpileTop :: Top -> String
-transpileTop (FunTop name args returnType body) = transpileFun (name, args, returnType, body)
-transpileTop (VarTop names type_ values) = transpileVar (names, type_, values) ++ ";"
-transpileTop (IncludeTop includes) = unlines $ ("#include " ++) <$> includes
-
-transpileLit :: Lit -> String
-transpileLit (IntLit i) = show i
-transpileLit (BoolLit True) = "true"
-transpileLit (BoolLit False) = "false"
-transpileLit (StringLit s) = "\"" ++ s ++ "\""
-
-transpileCall :: [Expr] -> String
-transpileCall (fun : args) = transpileExpr fun ++ "(" ++ (intercalate ", " $ transpileExpr <$> args) ++ ")"
-transpileCall _ = nonEx "transpileCall"
-
-transpileStat :: Stat -> String
-transpileStat (VarStat names type_ values) = transpileVar (names, type_, values)
-transpileStat (CallStat exprs) = transpileCall exprs
-
-transpileExpr :: Expr -> String
-transpileExpr (CallExpr sexps) = transpileCall sexps
-transpileExpr (LitExpr l) = transpileLit l
-transpileExpr (VarExpr v) = mangleString v
-
-transpileVar :: ([String], Sexp, [Maybe Expr]) -> String
-transpileVar ([name], type_, [Nothing]) = transpileType type_ ++ " " ++ mangleString name
-transpileVar ([name], type_, [Just value]) = transpileType type_ ++ " " ++ mangleString name ++ " = " ++ transpileExpr value
-transpileVar (names, type_, values) =
-    let
-        (strippedType, ptrs) = stripPointrs $ transpileType type_
-        zipped :: [(String, Maybe Expr)]
-        zipped = zip names values
-        mapRow :: (String, Maybe Expr) -> String
-        mapRow (name, Nothing) = replicate ptrs '*' ++ mangleString name
-        mapRow (name, Just expr) = replicate ptrs '*' ++ mangleString name ++ " = " ++ transpileExpr expr
-    in
-        strippedType ++ " " ++ (intercalate ", " (mapRow <$> zipped))
-
-transpileFun :: (String, [(String, Sexp)], Sexp, [Body]) -> String
-transpileFun (name, args, returnType, body) = transpileType returnType
-    ++ " " 
-    ++ mangleString name 
-    ++ " ("
-    ++ (intercalate ", " $ transpileArg <$> args)
-    ++ ") { " 
-    ++ transpileBodies body
-    ++ " }"
-        where
-            transpileArg :: (String, Sexp) -> String
-            transpileArg (argName, argType) = transpileType argType ++ " " ++ mangleString argName
-
-transpileBodies :: [Body] -> String
-transpileBodies bs = foldMap (++ "; ") $ transpileBody <$> bs
-
-transpileFor :: ((Maybe Stat), (Maybe Expr), (Maybe Stat), [Body]) -> String
-transpileFor (init, cond, update, body) = 
-    let
-        init' = fromMaybe "" (transpileStat <$> init)
-        cond' = fromMaybe "" (transpileExpr <$> cond)
-        update' = fromMaybe "" (transpileStat <$> update)
-    in
-        "for ("
-            ++ init'
-            ++ ";" 
-            ++ cond' 
-            ++ ";"
-            ++ update'
-            ++ ") { " 
-            ++ transpileBodies body
-            ++ "}" 
-
-transpileIf :: ([(Expr, [Body])], (Maybe [Body])) -> String
-transpileIf ((if_ : elseIfs), else_) =
-    let 
-        transpileIf :: (Expr, [Body]) -> String
-        transpileIf (expr, body) = "if (" ++ transpileExpr expr ++ ") { " ++ transpileBodies body ++ " } "
-        transpileElseIf :: (Expr, [Body]) -> String
-        transpileElseIf (expr, body) = "else if (" ++ transpileExpr expr ++ ") { " ++ transpileBodies body ++ " } "
-        transpileElse :: [Body] -> String
-        transpileElse b = "else { " ++ transpileBodies b ++ " }"
-    in
-        transpileIf if_
-            ++ (concat $ transpileElseIf <$> elseIfs) 
-            ++ (fromMaybe "" $ transpileElse <$> else_)
-transpileIf _ = nonEx "transpileIf"
-
-transpileBody :: Body -> String
-transpileBody (FunBody name args returnType body) = transpileFun (name, args, returnType, body)
-transpileBody (RetBody Nothing) = "return"
-transpileBody (RetBody (Just expr)) = "return " ++ transpileExpr expr
-transpileBody (VarBody names type_ values) = transpileVar (names, type_, values)
-transpileBody (IfBody conds else_) = transpileIf (conds, else_)
-transpileBody (ForBody init cond update body) = transpileFor (init, cond, update, body)
-transpileBody (CallBody exprs) = transpileCall exprs
-
-transpileAll :: [Top] -> String
-transpileAll tops = {- "#include <stdio.h>\n" ++ -} unlines $ transpileTop <$> tops
-
-tabs :: Int -> String
-tabs i = replicate i '\t'
-
-stripPointrs :: String -> (String, Int)
-stripPointrs = second length . break (== '*')
-
-mangleFun :: String -> String
-mangleFun s = "gsfun_" ++ mangleString s
-
-mangleVar :: String -> String
-mangleVar s = "gsvar_" ++ mangleString s
-
-mangleString :: String -> String
-mangleString = (>>= mangleChar)
-
-mangleChar :: Char -> String
-mangleChar c =
-    if c == '_' then 
-        "__"
-    else if ('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z') || ('0' <= c && c <= '9') || c == '_' then
-        [c]
-    else
-        "_" ++ (show $ ord c)
 
 (<<$>>) :: (Functor f, Functor g) => (a -> b) -> f (g a) -> f (g b)
 (<<$>>) = fmap . fmap
--- output.c
+++ output.c
@@ -1,8 +1,8 @@
 #include "stdio.h"
 #include "stdlib.h"
 
-int* xs = init();
+int* xs;
 int *x, *y, *z;
-int _43 (int x, int y) { for (Int z = x;_62(y, 0);_43_61(y, 1)) { inc(z); }; return z;  }
+int _43 (int x, int y) { for (int z = x;_62(y, 0);_43_61(y, 1)) { inc(z); }; return z;  }
 void forever () { for (;;) { };  }
 void main () { printf("asdf !!");  }
